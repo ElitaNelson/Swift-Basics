6678035e71422d126a13bdc0048f30374aba8e4c
Elita Nelson <ezn526@mail.missouri.edu>
2019-09-04 19:31:56 -0500

    First commit

---
+++ Swift Basics/Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Elita Nelson on 9/4/19.
+//  Copyright © 2019 Elita Nelson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
